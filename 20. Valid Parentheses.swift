23cde54421a7bb4d80818032f01cba49d09eb1af
Jesse <49723516+JesseBy@users.noreply.github.com>
2022-03-10 12:16:11 +0400

    Create 20. Valid Parentheses.swift

---
+++ 20. Valid Parentheses.swift
@@ -0,0 +1,49 @@
+let s = "()[]{}"
+//let s = "(]{}"
+
+class Solution {
+    
+    func isValid(_ s: String) -> Bool {
+        
+        if s.count % 2 != 0 {
+            print("false1")
+            return false
+        }
+        
+        var stack: [Character] = []
+        
+        for character in s {
+            switch character {
+            case "(": stack.append(")")
+            case "[": stack.append("]")
+            case "{": stack.append("}")
+            default:
+                if stack.isEmpty || stack.removeLast() != character {
+                    print("false2")
+                    return false
+                }
+            }
+        }
+        print(stack.isEmpty)
+        return stack.isEmpty
+    }
+    
+    
+//    func isValid(_ s: String) -> Bool {
+//        let array: [Character:Character] = [")":"(", "]":"[", "}":"{"]
+//        var stack: [Character] = []
+//
+//        for symbol in s {
+//            if let value = stack[symbol], stack.last == value {
+//                stack.popLast()
+//            } else {
+//                stack.append(symbol)
+//            }
+//        }
+//
+//        print(stack.isEmpty)
+//        return stack.isEmpty
+//    }
+}
+
+Solution().isValid(s)
