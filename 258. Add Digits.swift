7c5c24b8906140fdec96eb8358269eab26e52806
Jesse <49723516+JesseBy@users.noreply.github.com>
2022-03-10 12:24:24 +0400

    Create 258. Add Digits.swift

---
+++ 258. Add Digits.swift
@@ -0,0 +1,15 @@
+let num = 38
+
+class Solution {
+    func addDigits(_ num: Int) -> Int {
+        if num == 0 { return 0 }
+        if num % 9 == 0 { return 9 }
+        
+        print(num % 9)
+        return num % 9
+    }
+}
+
+Solution().addDigits(num)
+
+//i dont understand how it works
