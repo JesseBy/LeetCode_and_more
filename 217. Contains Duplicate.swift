16e219c33c8e7df7785f0b375241151b00701a71
Jesse <49723516+JesseBy@users.noreply.github.com>
2022-03-10 12:22:59 +0400

    Create 217. Contains Duplicate.swift

---
+++ 217. Contains Duplicate.swift
@@ -0,0 +1,23 @@
+
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        for i in 0..<nums.count {
+            for j in 0..<nums.count where i != j {
+                if nums[i] == nums[j] {return true}
+            }
+        }
+        return false
+    }
+}
+
+
+//class Solution {
+//    func containsDuplicate(_ nums: [Int]) -> Bool {
+//        for i in 0..<nums.count {
+//            for j in i+1..<nums.count {
+//                if nums[i] == nums[j] {return true}
+//            }
+//        }
+//        return false
+//    }
+//}
