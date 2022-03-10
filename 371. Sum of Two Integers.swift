fa601a4307976b45e4e302d5edc0f7dc0356cfcd
Jesse <49723516+JesseBy@users.noreply.github.com>
2022-03-10 12:28:03 +0400

    Create 371. Sum of Two Integers.swift

---
+++ 371. Sum of Two Integers.swift
@@ -0,0 +1,12 @@
+//let a = 1, b = 2
+let a = 2, b = 5
+
+class Solution {
+    func getSum(_ a: Int, _ b: Int) -> Int {
+        print(a.advanced(by: b))
+        return a.advanced(by: b)
+    }
+}
+
+Solution().getSum(a, b)
+
