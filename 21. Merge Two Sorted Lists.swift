e31a5a0f1574ed118df0ad1be55b2eac4fdc9ea6
Jesse <49723516+JesseBy@users.noreply.github.com>
2022-03-30 14:34:04 +0400

    Update 21. Merge Two Sorted Lists.swift

--- 21. Merge Two Sorted Lists.swift
+++ 21. Merge Two Sorted Lists.swift
@@ -1,17 +1,17 @@
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-        if(list1 == nil) {return list2}
-        if(list2 == nil) {return list1}
+        if(list1 == nil) { return list2 }
+        if(list2 == nil) { return list1 }
         var result:ListNode? = nil
         
         if(list1!.val <= list2!.val) {
             result = list1
-            result!.next = mergeTwoLists(list1!.next,list2)
-        }
-        else {
+            result!.next = mergeTwoLists(list1!.next, list2)
+        } else {
             result = list2
-            result!.next = mergeTwoLists(list1,list2!.next)
+            result!.next = mergeTwoLists(list1, list2!.next)
         }
+        
         return result
     }
 }
