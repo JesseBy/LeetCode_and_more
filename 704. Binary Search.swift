320e1acb27d36678eb6d66562b08cca77161bc37
Jesse <49723516+JesseBy@users.noreply.github.com>
2022-04-11 14:00:36 +0400

    Update 704. Binary Search.swift

--- 704. Binary Search.swift
+++ 704. Binary Search.swift
@@ -1,22 +1,49 @@
-//let nums = [-1,0,3,5,9,12], target = 9
-let nums = [-1,0,3,5,9,12], target = 2
+let nums = [-1, 0, 3, 5, 9, 12], target = 9 //4
 
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        //if nums.count == 0 { return -1 }
-        var cc = 0
+        guard !nums.isEmpty else { return -1 }
         
-        for i in nums {
-            if i == target {
-                print(cc)
-                return cc
+        var left = 0
+        var right = nums.count - 1
+        
+        while left <= right {
+            let mid = left + ((right - left) / 2)
+            let midValue = nums[mid]
+            
+            if midValue == target { return mid }
+            
+            if target > nums[mid] {
+                left = mid + 1
             } else {
-                cc += 1
+                right = mid - 1
             }
         }
-        print("-1")
+        
         return -1
     }
 }
 
-Solution().search(nums, target)
+
+
+//class Solution {
+//    func search(_ nums: [Int], _ target: Int) -> Int {
+//        //if nums.count == 0 { return -1 }
+//        var cc = 0
+//
+//        for i in nums {
+//            if i == target {
+//                print(cc)
+//                return cc
+//            } else {
+//                cc += 1
+//            }
+//        }
+//
+//        print("-1")
+//        return -1
+//    }
+//}
+
+//Solution().search(nums, target)
+print(Solution().search(nums, target))
